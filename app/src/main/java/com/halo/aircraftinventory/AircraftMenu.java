caee0b0ae622d07852b85f6c05a38fbdb00baa47
Diego Ramírez <diegormz97@outlook.com>
2018-10-28 04:41:51 -0600

    Fragments implemented

--- app/src/main/java/com/halo/aircraftinventory/AircraftMenu.java
+++ app/src/main/java/com/halo/aircraftinventory/AircraftMenu.java
@@ -1,38 +1,55 @@
 package com.halo.aircraftinventory;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class AircraftMenu extends AppCompatActivity {
+import com.halo.aircraftinventory.fragment.MenuFragment;
+
+public class AircraftMenu extends AppCompatActivity implements MenuFragment.OnFragmentInteractionListener{
+    MenuFragment menuFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aircraft_menu);
+
+        menuFragment = new MenuFragment();
+
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+        ft.add(R.id.menuFrame,menuFragment);
+
+        ft.commit();
     }
 
     public void gotoInsertAircraft(View view){
-        Intent intent = new Intent(this,InsertAircraft.class);
-
-        startActivity(intent);
+        menuFragment.liftInsert(this);
     }
 
     public void gotoConsultAircraft(View view){
-        Intent intent = new Intent(this,ViewAircraft.class);
-
-        startActivity(intent);
+        menuFragment.liftShow(this);
     }
 
     public void gotoUpdateAircraft(View v){
-        Intent i = new Intent(this,UpdateAircraft.class);
-
-        startActivity(i);
+        menuFragment.liftUpdate(this);
     }
 
     public void gotoDeleteAircraft(View v){
-        Intent i = new Intent(this,DeleteAircraft.class);
-        startActivity(i);
+        menuFragment.liftDelete(this);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
